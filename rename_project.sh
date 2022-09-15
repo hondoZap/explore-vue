c6308ec320bc04fdf86e8c93dc1345c2244792cd
Shamus Taylor <jtaylor@labattfood.com>
2022-09-15 11:08:47 -0500

    Script to rename project

--- rename_project.sh
+++ rename_project.sh
@@ -1,27 +1,50 @@
 #!/bin/bash -e
 
+self_name='rename_project.sh'
+
 main() {
   read -p 'Enter the context name (kebab-case): ' context_name
   read -p 'Enter the solution name (PascalCase): ' solution_name
   read -p 'Enter the human-readable app name (My App): ' app_name
 
-  replace_in_all_files "project-template" "$context_name"
-  replace_in_all_files "ProjectTemplate" "$solution_name"
-  replace_in_all_files "Project_Template" "$app_name"
+  replace_in_all_files 'project-template' "$context_name"
+  replace_in_all_files 'ProjectTemplate' "$solution_name"
+  replace_in_all_files 'Project_Template' "$app_name"
 
-  replace_in_all_file_names "ProjectTemplate" "$solution_name"
+  replace_in_all_file_names 'ProjectTemplate' "$solution_name"
 
-  rm rename_project.sh
+  rm "$self_name"
 
   git add --all && git commit -m 'Rename project files'
 }
 
 replace_in_all_files() {
-  not_implemented
+  local replace_from="$1"
+  : "${replace_from:?}"
+  local replace_to="$2"
+  : "${replace_to:?}"
+
+  for file in $(git ls-files); do
+    if [[ "$file" != "$self_name" ]]; then
+      sed -i -e "s/$replace_from/$replace_to/g" "$file"
+    fi
+  done
 }
 
 replace_in_all_file_names() {
-  not_implemented
+  local replace_from="$1"
+  : "${replace_from:?}"
+  local replace_to="$2"
+  : "${replace_to:?}"
+
+  for file in $(git ls-files | grep "$replace_from"); do
+    local new_name="${file//"$replace_from"/"$replace_to"}"
+    mkdir -p "$(dirname "$new_name")"
+    mv "$file" "$new_name"
+  done
+
+  find . -type d -empty -delete
 }
 
 main
+
