46611df9aabe556894d7d9072cd38b868b7d113e
Hondo Zapien <azapien@labattfood.com>
2023-08-24 09:27:42 -0500

    Add carousel

--- project-template-ui/src/views/MarketPlaceView.vue
+++ project-template-ui/src/views/MarketPlaceView.vue
@@ -1,6 +1,19 @@
 <template>
   <h1>MarketPlace</h1>
-  <h2>Welcome {{ user }}</h2>
+  <v-carousel>
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      cover
+    />
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+      cover
+    />
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      cover
+    />
+  </v-carousel>
 </template>
 
 <script lang="ts" setup>
